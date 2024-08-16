fbefd17a08571e0fafed5105b442260229892ba6
Abhijeet Selukar <abhijeetselukar118@gmail.com>
2024-08-16 15:34:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    version='0.0.1',    
+     author='Abhijeet',
+     author_email='abhijeetselukar118@gmail.com',  
+    packages=find_packages(),
+    install_requires=[],
+)
